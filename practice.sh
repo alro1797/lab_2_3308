ceaba73b046779cf196b6f2ffe7afe09709c8e22
alro1797 <alro1797@colorado.edu>
2019-01-30 15:48:40 -0700

    Creating practice.sh file & storing regex_practice.sh

---
+++ practice.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Allison Roney
+# 1/30/2019
+
+echo "Enter a number: "
+read numOne
+
+echo "Enter a second number: "
+read numTwo
+
+sum=$(($numOne + $numTwo))
+echo "The sum is: $sum"
+
+let prod=numOne*numTwo
+echo "The product is: $prod"
+
+echo "File Name: $0"
+echo "Command Line Argument 1: $1"
+
+grep $1 $2
+
